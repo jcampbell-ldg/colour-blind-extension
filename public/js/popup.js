92a20c2422303ff0a6981a2758ed7ac8f7aa39c8
dfallows <dominicfallows@limitlessdigital.com>
2019-01-29 17:05:21 +0000

    added help section

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,66 +1,83 @@
-$(document).ready(function(){
-    chrome.storage.sync.get('new_user', function(items){
-        if(items['new_user']){
-            alert('test');
+$(document).ready(function () {
+
+    for (var x = 1; x < 6; x++) {
+        $('#content' + x).hide();
+    }
+    chrome.storage.sync.get('new_user', function (items) {
+        if (items['new_user']) {
         } else {
             firstTimeSetup();
         }
     });
 
 
-    $('.tab').click(function(){
+    $('.tab').click(function () {
         var contentId = (this.id).slice(0, -4);
         $('.content').hide();
-        $('#content_'+contentId).show();
+        $('#content_' + contentId).show();
         $('.active').removeClass('active');
         $(this).addClass('active');
     });
 
     $('.colour_changer_options_tab:lt(3)').show();
 
-    $('#colour_changer_tab_left').click(function(){
+    $('#colour_changer_tab_left').click(function () {
         var id = $('#colour_changer_tab_left').nextAll('.colour_changer_options_tab:visible').attr('id');
         var id_count = parseInt(id.slice(-1));
         var total_count = $('.colour_changer_options_tab').length;
 
-        if(id_count > 0){
+        if (id_count > 0) {
             $('.colour_changer_options_tab').hide();
             $('.colour_changer_options_tab').slice(id_count - 1, id_count + 2).show();
         }
     });
 
-    $('#colour_changer_tab_right').click(function(){
+    $('#colour_changer_tab_right').click(function () {
         var id = $('#colour_changer_tab_left').nextAll('.colour_changer_options_tab:visible').attr('id');
         var id_count = parseInt(id.slice(-1));
         var total_count = $('.colour_changer_options_tab').length;
 
-        if(id_count < (total_count - 3)){
+        if (id_count < (total_count - 3)) {
             $('.colour_changer_options_tab').hide();
             $('.colour_changer_options_tab').slice(id_count + 1, id_count + 4).show();
         }
     });
 
-    $('.colour_changer_options_tab').click(function(){
+    $('.colour_changer_options_tab').click(function () {
         $('.colour_active').removeClass('colour_active');
         $(this).addClass('colour_active');
 
         previewColours(this.id);
     });
 
-    $('#colour_changer_apply').click(function(){
+    $('#colour_changer_apply').click(function () {
         setColour();
     });
 
     $('.config').on('click', loadConfig);
+
+    $('.drop_down_tip').click(function () {
+        helpTab($(this));
+    });
 
     loadOptions();
     // setData();
     // console.log(getConfig());
 });
 
-function previewColours(id)
-{
-    var element = $('#'+id);
+function helpTab(element) {
+    var id = element.attr("id");
+    if (element.attr('class') === 'drop_down_tip') {
+        element.removeClass("drop_down_tip").addClass('drop_down_tip_open');
+        $('#content' + id).show();
+    } else {
+        $('#content' + id).hide();
+        element.removeClass("drop_down_tip_open").addClass('drop_down_tip');
+    }
+}
+
+function previewColours(id) {
+    var element = $('#' + id);
     var backgroundColour = element.data('background');
     var fontColour = element.data('font');
     var borderColour = element.data('border');
@@ -94,8 +111,7 @@ function setColour() {
     });
 }
 
-function loadOptions()
-{
+function loadOptions() {
     var div;
     chrome.storage.sync.get(null, function (items) {
         for (key in items) {
@@ -110,11 +126,10 @@ function loadOptions()
     });
 }
 
-function loadConfig()
-{
+function loadConfig() {
     var id = (this.id.substr(-8));
-    $('#content_custom_design_settings_form').load('/views/custom_design_form.html', function(){
-        $('#custom_design_submit').click(function(){
+    $('#content_custom_design_settings_form').load('/views/custom_design_form.html', function () {
+        $('#custom_design_submit').click(function () {
             saveConfig();
         });
 
@@ -128,8 +143,7 @@ function loadConfig()
     });
 }
 
-function saveConfig()
-{
+function saveConfig() {
     var id = $('#custom_design_id').val();
     var name = $('#custom_design_name').val();
     var background = $('#custom_design_background_colour').val();
@@ -139,10 +153,10 @@ function saveConfig()
     var config = {};
     config[id] = {name: name, bgColour: background, fontColour: font, borderColour: border};
 
-    chrome.storage.sync.get(id, function(items){
-        if(items[id] != undefined){
-            chrome.storage.sync.remove(id, function(){
-                chrome.storage.sync.set(config, function(){
+    chrome.storage.sync.get(id, function (items) {
+        if (items[id] != undefined) {
+            chrome.storage.sync.remove(id, function () {
+                chrome.storage.sync.set(config, function () {
                     //SET SUCCESS MESSAGE
                     console.log('Successfully Saved');
                 });
@@ -151,29 +165,30 @@ function saveConfig()
     })
 }
 
-function checkForNewUser()
-{
-
+function checkForNewUser() {
 
 
 }
 
 //First Time Setup
-function firstTimeSetup()
-{
+function firstTimeSetup() {
     chrome.storage.sync.clear();
     setConfigs();
 }
 
 //Sets the predefined configs
-function setConfigs()
-{
+function setConfigs() {
     chrome.storage.sync.set({
         config_1: {name: 'Deuteranopia', bgColour: '#efefff', fontColour: '#252525', borderColour: '#252525'},
         config_2: {name: 'Protanopia', bgColour: '#e8e8db', fontColour: '#3d3d9b', borderColour: '#3d3d9b'},
         config_3: {name: 'Tritanopia', bgColour: '#fbdee2', fontColour: '#252525', borderColour: '#252525'},
         config_4: {name: 'Protanopia/Tritanopia', bgColour: '#b6b690', fontColour: '#3d3d9b', borderColour: '#3d3d9b'},
-        config_5: {name: 'Deuteranopia/Tritanopia', bgColour: '#edf3f7', fontColour: '#a60037', borderColour: '#a60037'},
+        config_5: {
+            name: 'Deuteranopia/Tritanopia',
+            bgColour: '#edf3f7',
+            fontColour: '#a60037',
+            borderColour: '#a60037'
+        },
         new_user: true
     });
 }
